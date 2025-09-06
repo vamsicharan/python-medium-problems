2052e617b414ccb2019a49f28e72b17e83342f9b
vamsi_charan <vamsicharan585@gmail.com>
2025-09-06 21:32:38 +0530

    peak value

---
+++ peaknumber.py
@@ -0,0 +1,14 @@
+# Find Peak Element
+
+# Problem: Write a function to find a peak element in an array. An element is a peak if it is not smaller than its neighbours.
+# Testcase 1:
+# Input: [1, 3, 20, 4, 1, 0]
+# Output: 20
+def values():
+        nums = [1, 10, 2, 4, 3, 5]
+        peak = 0
+        for i in range(1,len(nums)-1):
+            if  nums[i-1] < nums[i] > nums[i+1]:
+                peak = nums[i]
+        return peak
+print(values())  
